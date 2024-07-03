a026db96b6969ac83474bb95838cf5d17474e4fd
LeandroSinich <leandrosinich@gmail.com>
2024-07-03 12:24:23 -0300

    house-detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -9,11 +10,15 @@ import { HomeComponent } from './home/home.component';
       <img src="/assets/logo.svg" alt="logo" class="band-logo" aria-hidden="true">
     </header>
     <section>
-      <app-home></app-home>
+    <form action="">
+      <input type="text" placeholder="filter by city">
+      <button type="button" class="primary">Search</button>
+    </form>
+    <router-outlet />
     </section>
   </main>`,
   styleUrls: ['./app.component.css'],
-  imports: [HomeComponent]
+  imports: [HomeComponent, RouterOutlet, RouterLink]
 })
 export class AppComponent {
   title = 'homes';
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -11,6 +11,7 @@ import { HousingLocation } from '../housing-location';
       <img [src]="housingLocation.photo" alt="exterior photo of {{housingLocation.name}}" class="listing-photo">
       <h2 class="listing-heading">{{housingLocation.name}}</h2>
       <p class="listing-location">{{housingLocation.city}}, {{housingLocation.state}}</p>
+      <a href="">Learn more</a>
     </section>
   `,
   styleUrls: ['./housing-location.component.css']
